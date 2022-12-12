e7b8139637dc738f8606673d40ac518c68fef24e
alberthli <alberthli@berkeley.edu>
2022-12-11 18:55:54 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pinocchio",
+    version="0.0.1",
+    author="Albert Li",
+    author_email="alberthli@caltech.edu",
+    description="Pinocchio fork, Dec 11, 2022.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alberthli/pinocchio-frozen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.10",
+    license="MIT",
+    # install_requires=[
+    #     "numpy",
+    # ],
+)
