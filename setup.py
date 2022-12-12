03bed9b72ec530cd4e28425ae48c5b57fb311736
alberthli <alberthli@berkeley.edu>
2022-12-11 20:20:34 -0800

    try

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/alberthli/pinocchio-frozen",
     packages=setuptools.find_packages(
-        where="bindings/python/pinocchio",
+        where="bindings/python",
         include=["pinocchio"],
     ),
-    package_dir={"pinocchio": "bindings/python"},
+    # package_dir={"pinocchio": "bindings/python"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
